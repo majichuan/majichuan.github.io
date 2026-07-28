d5d60ee94f2ed8549b2964ee24c57c87116a7365
ma-ji-chuan <18509717089@163.com>
2026-07-28 18:02:25 +0800

    personal website init

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const base = 'C:/Users/Alex/personal-website';
+http.createServer((req, res) => {
+  const filePath = path.join(base, req.url === '/' ? 'index.html' : req.url);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('404'); }
+    else { res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); res.end(data); }
+  });
+}).listen(3000);
